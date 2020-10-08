c9a4d14e6c7bfd45cd036bb48c5aa20fb91f4128
Samyna <samyna.nourdine@hotmail.com>
2020-10-08 18:46:19 +0200

    remove map

--- js/Map.js
+++ js/Map.js
@@ -1,48 +1,51 @@
 // strict mode
-
 " use strict";
 
  // creation de l'objet Map
 
 class Map {
-    constructor(lat, long) {
+    constructor(lat, lng) {
         this.lat = lat;
-        this.long = long;
-
+        this.lng = lng;
+        this.zoom= 14;
     }
-
 // initialisation de la Carte
 
     initMap() {
-
-        let mymap = L.map("mapid").setView([this.lat, this.long], 10);
+        let mymap = L.map("mapid").setView([this.lat, this.lng], this.zoom);
 // initialisation des tuiles
         L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            minZom: 1,
+            minZom: 14,
             maxZoom: 18,
+            zoomDelta: 0.25,
+            zoomSnap: 0,
             id: "mapbox/streets-v11",
             tileSize: 512,
             zoomOffset: -1,
             accessToken: "pk.eyJ1Ijoic2FteW5hIiwiYSI6ImNrZWNzN242MTBsdGgyenBnbDFhcXRycmoifQ.KW6ntl4LiqCbqkQgFMWFoA"
         }).addTo(mymap);
 
-        //---variables de déclaration
-
-        const form = document.querySelector("form");
-        const title = document.getElementById("title");
-        const address = document.getElementById("address");
-        const places = document.getElementById("places");
-        const bikes = document.getElementById("bikes");
-        const dispos = document.getElementById("disponibility");
-
-// on appelle la fonction ajax
+        // on recupere les stations
 
         ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=a1bb8a2d6bce79ce80f96e68686478140bfc9616 ", function (reponse) {
-            let stations = JSON.parse(reponse);
-            stations.forEach(function (station) {
+            console.log(reponse);
+            let stationsList = JSON.parse(reponse);
+            console.log(stationsList);
 
-                let pop = function () {
+            //Affichage des infos de la station lors d'un clic sur son marker
+            stationsList.forEach(function (station) {
+                console.log(station);
+                //---variables de déclaration
+
+                const form = document.querySelector("form");
+                const title = document.getElementById("title");
+                const address = document.getElementById("address");
+                const places = document.getElementById("places");
+                const bikes = document.getElementById("bikes");
+                const dispos = document.getElementById("disponibility");
+
+                 let pop = function () {
 
                     form.classList.replace("infos", "showinfo");
                     marker.bindPopup(station.name).openPopup();
@@ -53,36 +56,54 @@ class Map {
                     places.innerHTML = "<strong>Place(s) disponible(s)</strong> : " + station.available_bike_stands;
                     bikes.innerHTML = "<strong>Vélo(s) disponible(s)</strong> : " + station.available_bikes;
 
-
                     if (station.available_bikes === 0) {
                         dispo.classList.replace("dispo_on", "dispo_off");
-                    } else {
+                    }
+                    else {
                         dispo.classList.replace("dispo_off", "dispo_on");
                     }
-
-
-                };
-                //---CREATE N0 DISPO MARKER
+                }
+                // creation des markers
 
                 let redMarker = L.icon({
-                    iconUrl: "images/redMarker.png",
-                    iconSize: [25, 38],
-                    iconAnchor: [22, 94],
-                    popupAnchor: [-10, -95]
+                    iconUrl: "images/redMark.png",
+                    iconSize: [35, 35],
+                    shadowSize:   [70, 70],
+                    iconAnchor: [25, 50],
+                    shadowAnchor: [0, 0],
+                    popupAnchor: [-5, -40]
                 });
 
-                //--creation des markers
+                let greenMarker= L.icon({
+                    iconUrl: "images/greenMark.png",
+                    shadowSize:   [70, 70],
+                    iconSize: [35, 35],
+                    iconAnchor: [22, 50],
+                    shadowAnchor: [0, 0],
+                    popupAnchor: [-5, -40]
+                });
 
+                let marker = "";
+
+                if (station.status === "OPEN") {
+                    marker = L.marker([station.position.lat, station.position.lng], {icon:greenMarker }).addTo(mymap);
+                }
+                else {
+                    marker = L.marker([station.position.lat, station.position.lng], {icon: redMarker}).addTo(mymap);
+                }
+                marker.bindPopup(station);
                 if (station.available_bikes !== 0) {
                     let marker = L.marker(station.position).on("click", pop).addTo(mymap);
-                } else {
+                }
+                else {
                     let marker = L.marker(station.position, {icon: redMarker}).on("click", pop).addTo(mymap);
                 }
-
-
 
             });
         });
 
     };
 }
+let mapLyon = new Map(45.7578137,4.8320114);
+mapLyon.initMap()
+
